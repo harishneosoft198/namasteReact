6899a622d8300837c46651e93dbc13db578b0439
Harish <harish.krishnapuram@neosoftmail.com>
2023-12-06 15:25:40 +0530

    parallel sibling creating in nested elements

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 //creating elements using the React
-var heading = React.createElement('h1',{id:'heading'},'Hello World From React');//returns the js object
+var heading = React.createElement('h1', { id: 'heading' }, 'Hello World From React');//returns the js object
 var root = ReactDOM.createRoot(document.getElementById('root'));//renders the html element using object so browser will understand
 root.render(heading);
 
-//nesting in react
-var parent = React.createElement('div',{id:'parent'},React.createElement('div',{id:'child'},React.createElement('h1',{id:'heading1'},'Hi this is nested heading')));
+//nesting in react and parallel siblings
+var parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'Hi this is nested heading'),React.createElement('h2', { }, 'Hi this is nested heading2')]));
 root.render(parent);
